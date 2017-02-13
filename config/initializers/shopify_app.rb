a4c0c9bc0172a0de7d442ec5a30909da0d52d3d4
13LD <tompla96@ukr.net>
2017-02-13 23:57:16 +0200

    fixed api_key

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,6 +1,6 @@
 ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
-  config.api_key = "<api_key>"
+  config.api_key = "d3923c4e2d14b94043678c71ffdd3b94"
   config.secret = "8cca47f20e0fba81c4ce992eea69816f"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
