8954127864170e2c40827e9e2fefd96af0e05397
13LD <tompla96@ukr.net>
2017-02-13 23:35:41 +0200

    fixes

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,7 +2,7 @@ ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
   config.api_key = "<api_key>"
   config.secret = "8cca47f20e0fba81c4ce992eea69816f"
-  config.redirect_uri = "http://shoptests.herokuapp.com/"
+  config.redirect_uri = "http://shoptests.herokuapp.com/auth/shopify/callback"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
